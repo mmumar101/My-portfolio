89f0db375f073be36f15f2a567b7c7a8c3b1cdc5
Mohammed <mmumar101@gmail.com>
2023-10-16 05:17:18 -0700

    deployed website

--- src/components/projects.js
+++ src/components/projects.js
@@ -3,6 +3,9 @@ import logo1 from '../images/clock.png'
 import logo2 from '../images/Fylo.png'
 import logo3 from '../images/game.png'
 import logo4 from '../images/timer.png'
+import logo5 from '../images/todo.png'
+import logo6 from '../images/dictionary.png'
+
 
 const projects = () => {
   return (
@@ -16,6 +19,49 @@ const projects = () => {
         </div>
 
         <div className='grid sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 '>
+                  {/*DICTIONARY APP */}
+                  <div className='mt-4'>
+                  <div className='mb-4'>
+                    <img src={logo6} alt='Dictionary' className='h-25 w-25'/>
+                    {/* <iframe title='Analog' src='https://mmumar101.github.io/My-Fylo-Landing-Page/ width={500} height={500]'></iframe> */}
+                  </div>
+                  <div className='text-[22px] font-bold'>DICTIONARY APP</div>
+
+                  <div className='flex gap-5 pt-2'>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                    <p>JavaScript</p>
+                    <p>React</p>
+                  </div>
+
+                  <div className='flex font-500 gap-7 py-6'>
+                    <a href='https://mmumar101.github.io/Dictionary/' className='border-b-2 border-[#4ee180]'>VIEW PROJECT</a>
+                    <a href='https://github.com/mmumar101/Dictionary' className='border-b-2 border-[#4ee180]'>VIEW CODE</a>
+                  </div>
+              </div>
+
+
+                {/*TODO APP */}
+                    <div className='mt-4'>
+                  <div className='mb-4'>
+                    <img src={logo5} alt='Todo' className='h-25 w-25'/>
+                    {/* <iframe title='Analog' src='https://mmumar101.github.io/My-Fylo-Landing-Page/ width={500} height={500]'></iframe> */}
+                  </div>
+                  <div className='text-[22px] font-bold'>TODO APP</div>
+
+                  <div className='flex gap-5 pt-2'>
+                    <p>HTML</p>
+                    <p>CSS</p>
+                    <p>JavaScript</p>
+                    <p>React</p>
+                  </div>
+
+                  <div className='flex font-500 gap-7 py-6'>
+                    <a href='https://mmumar101.github.io/Todo-App/' className='border-b-2 border-[#4ee180]'>VIEW PROJECT</a>
+                    <a href='https://github.com/mmumar101/Todo-App' className='border-b-2 border-[#4ee180]'>VIEW CODE</a>
+                  </div>
+              </div>
+
               {/* ANALOG CLOCK */}
               <div className='mt-4 pr-4'>
                   <div className='mb-4'>
